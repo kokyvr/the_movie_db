dc40c4a4f697cf50b3f6028856629905e4869295
Jorge Ventura <venturakoky12@gmail.com>
2023-08-18 19:57:55 -0500

    add consume api

--- src/main/java/com/the/movie/db/utils/ConsumeApi.java
+++ src/main/java/com/the/movie/db/utils/ConsumeApi.java
@@ -1,5 +1,14 @@
 package com.the.movie.db.utils;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
+
 public class ConsumeApi<T>{
 
+    @Autowired
+    private RestTemplate restTemplate;
+
+     public T getApi(String url,Class<T> type){
+         return this.restTemplate.getForEntity(url,type).getBody();
+    }
 }
