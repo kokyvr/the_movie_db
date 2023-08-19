34c9ce0997f72633d0649a9a9060bce880c07b68
Jorge Ventura <venturakoky12@gmail.com>
2023-08-18 19:59:47 -0500

    add logic business moviedb

--- src/main/java/com/the/movie/db/service/impl/MovieServiceImpl.java
+++ src/main/java/com/the/movie/db/service/impl/MovieServiceImpl.java
@@ -2,10 +2,16 @@ package com.the.movie.db.service.impl;
 
 import com.the.movie.db.dto.MovieDTO;
 import com.the.movie.db.service.MovieService;
+import com.the.movie.db.utils.ConsumeApi;
+import org.springframework.beans.factory.annotation.Autowired;
 
+@Autowired
 public class MovieServiceImpl implements MovieService {
+
+    @Autowired
+    private ConsumeApi<MovieDTO> restTemplate;
     @Override
     public MovieDTO getAll(String url) {
-        return null;
+        return this.restTemplate.getApi(url,MovieDTO.class);
     }
 }
